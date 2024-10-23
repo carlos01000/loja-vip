15b5a2dcbb64f066f87774fbed3115a1fd4e0e85
carlos01000 <157937314+carlos01000@users.noreply.github.com>
2024-10-23 17:57:01 -0300

    loja-vip

---
+++ _next/static/chunks/app/categories/%5Bid%5D/page-4c9bbe1863282f1e.js/page-4c9bbe1863282f1e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[326],{207:function(e,n,s){Promise.resolve().then(s.t.bind(s,231,23)),Promise.resolve().then(s.bind(s,5621)),Promise.resolve().then(s.bind(s,5184)),Promise.resolve().then(s.bind(s,9377)),Promise.resolve().then(s.bind(s,6014))}},function(e){e.O(0,[676,868,484,632,251,971,23,744],function(){return e(e.s=207)}),_N_E=e.O()}]);
